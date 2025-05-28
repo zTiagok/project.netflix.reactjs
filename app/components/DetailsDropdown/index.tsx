46374a64acf06b5acc294382fbd3a6df1e7fa05e
Tiago Braga Costa <ztiagok@gmail.com>
2025-05-28 20:51:22 -0300

    style: Changing h3 element css and fitting FAQ css to replica site.

--- app/components/DetailsDropdown/index.tsx
+++ app/components/DetailsDropdown/index.tsx
@@ -7,12 +7,12 @@ type DetailsDropdownProps = {
 
 export default function DetailsDropdown(props: DetailsDropdownProps) {
   return (
-    <details className="h-full w-full bg-neutral-700/60">
-      <summary className="flex list-none justify-between p-5 text-lg font-bold">
+    <details className="h-full w-full bg-neutral-700/60 text-2xl">
+      <summary className="flex list-none justify-between border-b border-neutral-950 p-5">
         {props.title}
         <Plus size={32} />
       </summary>
-      <div className="px-5 pb-5">{props.children}</div>
+      <div className="px-5 py-5">{props.children}</div>
     </details>
   );
 }
