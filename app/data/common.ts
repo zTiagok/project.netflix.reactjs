d955ba0ce38cef67034b2c0a61100fa7242fd165
Tiago Braga Costa <ztiagok@gmail.com>
2025-05-30 13:17:06 -0300

    feat: Adding "movie_cards" into object.

--- app/data/common.ts
+++ app/data/common.ts
@@ -143,6 +143,98 @@ const common = {
       url: "",
     },
   ],
+  movie_cards: [
+    {
+      title: "Dept.Q",
+      description:
+        "A brash but brilliant cop becomes head of a new police department, where he leads an unlikely team of misfits in solving Edinburgh's cold cases.",
+      tags: ["2025", "A16", "Show", "Thrillers", "Mysteries", "Dramas"],
+      logo: "/app/assets/movies/deptq/logo.jpg",
+      hero: "/app/assets/movies/deptq/hero.jpg",
+      thumbnail: "/app/assets/movies/deptq/thumbnail.jpg",
+    },
+    {
+      title: "A Widow's Game",
+      description:
+        "When a man is found dead, the investigation shatters his widow's perfect facade and exposes a hidden double life in this thriller based on real events.",
+      tags: ["2025", "A16", "Movie", "Thrillers", "Mysteries", "Drama"],
+      logo: "/app/assets/movies/a_widows_game/logo.jpg",
+      hero: "/app/assets/movies/a_widows_game/hero.jpg",
+      thumbnail: "/app/assets/movies/a_widows_game/thumbnail.jpg",
+    },
+    {
+      title: "You",
+      description:
+        "A dangerously charming, obsessive man goes to extreme measures to insert himself into the lives of women who fascinate him.",
+      tags: ["2025", "18", "Show", "Thrillers", "Dramas"],
+      logo: "/app/assets/movies/you/logo.jpg",
+      hero: "/app/assets/movies/you/hero.jpg",
+      thumbnail: "/app/assets/movies/you/thumbnail.jpg",
+    },
+    {
+      title: "Bad Influence",
+      description:
+        "An ex-con gets a fresh start when hired to protect a wealthy heiress from a stalker — but their chemistry is hard to resist as they grow closer.",
+      tags: ["2025", "A14", "Movie", "Thrillers", "Romance", "Dramas"],
+      logo: "/app/assets/movies/bad_influence/logo.jpg",
+      hero: "/app/assets/movies/bad_influence/hero.jpg",
+      thumbnail: "/app/assets/movies/bad_influence/thumbnail.jpg",
+    },
+    {
+      title: "Fear Street: Prom Queen",
+      description:
+        "Who will be voted queen at Shadyside High's 1988 prom? For underdog Lori, competition is cutthroat even before someone starts killing off the candidates.",
+      tags: ["2025", "A18", "Movie", "Horror"],
+      logo: "/app/assets/movies/fear_street/logo.jpg",
+      hero: "/app/assets/movies/fear_street/hero.jpg",
+      thumbnail: "/app/assets/movies/fear_street/thumbnail.jpg",
+    },
+    {
+      title: "Exterritorial",
+      description:
+        "When her son vanishes inside a US consulate, ex-special forces soldier Sara does everything in her power to find him — and uncovers a dark conspiracy.",
+      tags: ["2025", "A16", "Movie", "Action", "Thrillers"],
+      logo: "/app/assets/movies/exterritorial/logo.jpg",
+      hero: "/app/assets/movies/exterritorial/hero.jpg",
+      thumbnail: "/app/assets/movies/exterritorial/thumbail.jpg",
+    },
+    {
+      title: "Puss in Boots: The Last Wish",
+      description:
+        "When his legendary feats bring his nine lives down to one, Puss in Boots sets out to find a magical Wishing Star in his most daring adventure yet.",
+      tags: ["2022", "L", "Movie", "Comedies", "Action", "Kids"],
+      logo: "/app/assets/movies/puss_in_boots/logo.jpg",
+      hero: "/app/assets/movies/puss_in_boots/hero.jpg",
+      thumbnail: "/app/assets/movies/puss_in_boots/thumbnail.jpg",
+    },
+    {
+      title: "Fast 'n Furious",
+      description:
+        "Dom Toretto faces his most dangerous challenge yet as he races to save his family from a nemesis with a vicious grudge and a serious case of road rage.",
+      tags: ["2023", "14", "Movie", "Action", "Thrillers", "Adventures"],
+      logo: "/app/assets/movies/fast_n_furious/logo.jpg",
+      hero: "/app/assets/movies/fast_n_furious/hero.jpg",
+      thumbnail: "/app/assets/movies/fast_n_furious/thumbnail.jpg",
+    },
+    {
+      title: "Last Bullet",
+      description:
+        "Car genius Lino returns to conclude his vendetta against Areski and the corrupt commander who ruined their lives in this turbo-charged trilogy finale.",
+      tags: ["2025", "A12", "Movie", "Action", "Thrillers"],
+      logo: "/app/assets/movies/last_bullet/logo.jpg",
+      hero: "/app/assets/movies/last_bullet/hero.jpg",
+      thumbnail: "/app/assets/movies/last_bullet/thumbnail.jpg",
+    },
+    {
+      title: "Havoc",
+      description:
+        "When a drug heist swerves lethally out of control, a jaded cop fights his way through a corrupt city's criminal underworld to save a politician's son.",
+      tags: ["2025", "A16", "Movie", "Action", "Thrillers"],
+      logo: "/app/assets/movies/havoc/logo.jpg",
+      hero: "/app/assets/movies/havoc/hero.jpg",
+      thumbnail: "/app/assets/movies/havoc/thumbnail.jpg",
+    },
+  ],
 };
 
 export default common;
