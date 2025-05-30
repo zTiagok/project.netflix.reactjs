7988a2fa5db298978d4b22dc5b71a7e95403d418
Tiago Braga Costa <ztiagok@gmail.com>
2025-05-30 13:19:13 -0300

    style: Movie card with pointer on hover and indexes cannot be selected.

--- app/components/MovieCard/index.tsx
+++ app/components/MovieCard/index.tsx
@@ -15,11 +15,11 @@ type MovieCardProps = {
 export default function MovieCard(props: MovieCardProps) {
   return (
     <div
-      className={`mx-8 flex h-full flex-col justify-end rounded-md bg-orange-500 bg-cover transition-transform duration-400 hover:scale-110`}
+      className={`mx-8 flex h-full flex-col justify-end rounded-md bg-orange-500 bg-cover transition-transform duration-400 hover:scale-110 hover:cursor-pointer`}
       style={{ backgroundImage: `url(${props.movie.thumbnail})` }}
     >
       <div
-        className={`relative -left-6 flex w-fit items-center justify-center text-[86px] font-[1000] text-white before:absolute before:flex-1 before:text-[79px] before:text-black before:content-[attr(data-number)]`}
+        className={`relative -left-6 flex w-fit items-center justify-center text-[86px] font-[1000] text-white select-none before:absolute before:flex-1 before:text-[79px] before:text-black before:content-[attr(data-number)]`}
         data-number={props.id + 1}
       >
         {props.id + 1}
