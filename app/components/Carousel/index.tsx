32ea9ba51f0e5b5aeef1eae7dbb26fe71cf9ed08
Tiago Braga Costa <ztiagok@gmail.com>
2025-05-30 13:17:29 -0300

    refactor: Carousel rendering movie cards.

--- app/components/Carousel/index.tsx
+++ app/components/Carousel/index.tsx
@@ -2,25 +2,28 @@ import "swiper/css";
 import "swiper/css/navigation";
 import { Navigation } from "swiper/modules";
 import { Swiper, SwiperSlide } from "swiper/react";
+import { common } from "~/data";
 import MovieCard from "../MovieCard";
 
 export default function Carousel() {
   return (
     <Swiper
-      spaceBetween={48}
       slidesPerView={7}
-      slidesOffsetBefore={48}
-      slidesOffsetAfter={48}
       speed={1000}
       slidesPerGroup={7}
       allowTouchMove={false}
       navigation
       modules={[Navigation]}
-      className="overflow-visible rounded-md bg-blue-500"
+      className="rounded-md"
     >
-      {Array.from({ length: 10 }).map((_data, index) => (
+      {/* {Array.from({ length: 10 }).map((_data, index) => (
         <SwiperSlide className="overflow-visible" key={`movie-card-${index}`}>
           <MovieCard id={index} key={`movie-card-${index}`} />
+        </SwiperSlide>
+      ))} */}
+      {common.movie_cards.map((data, index) => (
+        <SwiperSlide className="overflow-visible" key={`movie-card-${index}`}>
+          <MovieCard movie={data} id={index} key={`movie-card-${index}`} />
         </SwiperSlide>
       ))}
     </Swiper>
--- app/components/MovieCard/index.tsx
+++ app/components/MovieCard/index.tsx
@@ -1,10 +1,23 @@
+type MovieProps = {
+  title: string;
+  description: string;
+  tags: string[];
+  logo: string;
+  hero: string;
+  thumbnail: string;
+};
+
 type MovieCardProps = {
   id: number;
+  movie: MovieProps;
 };
 
 export default function MovieCard(props: MovieCardProps) {
   return (
-    <div className="flex h-full flex-col justify-end rounded-md bg-orange-500 transition-transform duration-400 hover:scale-110">
+    <div
+      className={`mx-8 flex h-full flex-col justify-end rounded-md bg-orange-500 bg-cover transition-transform duration-400 hover:scale-110`}
+      style={{ backgroundImage: `url(${props.movie.thumbnail})` }}
+    >
       <div
         className={`relative -left-6 flex w-fit items-center justify-center text-[86px] font-[1000] text-white before:absolute before:flex-1 before:text-[79px] before:text-black before:content-[attr(data-number)]`}
         data-number={props.id + 1}
