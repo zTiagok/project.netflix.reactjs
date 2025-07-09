5b29eaca8b371d392dd55674ce3e12ee0fdc87ee
Tiago Braga Costa <ztiagok@gmail.com>
2025-07-09 15:36:09 -0300

    feat: Installing react-hook-form and starting form validation

--- app/components/ReadyToWatch/index.tsx
+++ app/components/ReadyToWatch/index.tsx
@@ -1,8 +1,26 @@
 import { ChevronRight } from "lucide-react";
+import { useForm, type SubmitHandler } from "react-hook-form";
+
+interface FormInterface {
+  email: string;
+}
 
 export default function ReadyToWatch() {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<FormInterface>();
+
+  const onSubmit: SubmitHandler<FormInterface> = (data) => {
+    console.log(data);
+  };
+
   return (
-    <div className="flex flex-col items-center gap-3">
+    <form
+      className="flex w-fit flex-col items-center gap-3 self-center"
+      onSubmit={handleSubmit(onSubmit)}
+    >
       <span>
         Ready to watch? Enter your email to create or restart your membership.
       </span>
@@ -11,12 +29,21 @@ export default function ReadyToWatch() {
           type="email"
           className="min-w-[350px] rounded-sm border border-neutral-600 bg-neutral-900/70 px-4 py-3 text-lg"
           placeholder="Email address"
+          {...register("email", {
+            required: "Email is required.",
+            validate: (value) => value.includes("@") || "Email must be valid.",
+          })}
         />
         <button className="flex min-w-[200px] cursor-pointer items-center justify-center gap-2 rounded-sm bg-red-600 text-2xl text-white transition-colors hover:bg-red-700 active:bg-red-900">
           Get Started
           <ChevronRight />
         </button>
       </div>
-    </div>
+      {errors.email && (
+        <span className="self-start text-sm text-red-600">
+          {errors.email.message}
+        </span>
+      )}
+    </form>
   );
 }
